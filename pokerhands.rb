79acac3fa19af5330e93b7b556e2dd006441938f
Philip <info@jander.it>
2012-11-04 17:18:23 +0100

    ignoring acceptence tests, first red unit test

--- pokerhands.rb
+++ pokerhands.rb
@@ -1,6 +1,33 @@
 require 'minitest/autorun'
 
+
+Card = Struct.new(:value,:suit)
+
+CLUBS="C"
+SPADES="S"
+HEARTS="H"
+DIAMONDS="D"
+
+TWO="2"
+THREE="3"
+FOUR="4"
+FIVE="5"
+SIX="6"
+SEVEN="7"
+EIGHT="8"
+NINE="9"
+TEN="T"
+JACK="J"
+QUEEN="Q"
+KING="K"
+ACE="A"
+
+
+
 class PokerHandParser
+  def parseHand(hand)
+  end
+
   def parseBlackWhiteLine(line)
   end
 end
@@ -16,9 +43,13 @@ BLACK=1
 WHITE=2
 
 
+
 class PokerHandComparer
-  @parser = PokerHandParser.new
-  @evaluator = PokerHandEvaluator.new
+
+  def initialize(parser, evaluator)
+    @parser=parser
+    @evaluator=evaluator
+  end
 
   def compare(black, white)
     TIE
@@ -36,9 +67,22 @@ end
 
 
 describe PokerHandParser do
+  before do
+    @sut = PokerHandParser.new
+  end
+
+  it "parses a partial hand" do
+    @sut.parseHand("JS 5D").must_equal [Card.new(JACK,SPADES),Card.new(FIVE,DIAMONDS)]
+  end
+
 end
 
+
 describe PokerHandEvaluator do
+  before do
+    @sut = PokerHandEvaluator.new
+  end
+
 end
 
 
@@ -53,12 +97,12 @@ end
 
 describe PokerHandComparer do
   before do
-    @sut = PokerHandComparer.new
+    @sut = PokerHandComparer.new(PokerHandParser.new, PokerHandEvaluator.new)
   end
 
-  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", WHITE)
-  sample("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S", BLACK)
-  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH", BLACK)
-  sample("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH", TIE)
+#  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", WHITE)
+#  sample("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S", BLACK)
+#  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH", BLACK)
+#  sample("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH", TIE)
 
 end
