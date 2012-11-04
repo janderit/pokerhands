db8e93b769f26c9571374db8a3c53498797c3416
Philip <info@jander.it>
2012-11-04 16:59:19 +0100

    Kata specs

--- pokerhands.rb
+++ pokerhands.rb
@@ -1,1 +1,29 @@
 require 'minitest/autorun'
+
+
+
+class PokerHandComparer
+end
+
+
+
+
+def sample(line, expectation)
+  it "correctly analyses sample #{line}" do
+    @sut.analyse(line).must_equal expectation
+  end
+end
+
+
+
+describe PokerHandComparer do
+  before do
+    @sut = PokerHandComparer.new
+  end
+
+  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", WHITE)
+  sample("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S", BLACK)
+  sample("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH", BLACK)
+  sample("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH", TIE)
+
+end
